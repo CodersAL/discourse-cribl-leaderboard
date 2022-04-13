44f900be4867bf881c01e811d69b5835751fd1ad
Keegan George <kgeorge13@gmail.com>
2022-04-13 14:44:15 -0700

    DEV: Add leaderboard index route and redirect to todays

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Discourse::Application.routes.append do
 end
 
 ::CriblLeaderboard::Engine.routes.draw do
+  get '/' => 'request_data#todays'
   get '/todays' => 'request_data#todays'
   get '/todays/:user_id' => 'request_data#todays'
   get '/quarters' => 'request_data#quarters'
